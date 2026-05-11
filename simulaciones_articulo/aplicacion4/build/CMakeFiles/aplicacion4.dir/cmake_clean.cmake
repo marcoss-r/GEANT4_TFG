1d8469d4d47e797eb92db496886e532ebf64a11c
marcoss-r <marcos.salmeron@edu.uah.es>
2026-05-11 14:35:39 +0200

    feat: simulation data for article

---
+++ simulaciones_articulo/aplicacion4/build/CMakeFiles/aplicacion4.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/aplicacion4.dir/main.cc.o"
+  "CMakeFiles/aplicacion4.dir/src/ActionInit.cc.o"
+  "CMakeFiles/aplicacion4.dir/src/DetConstruction.cc.o"
+  "CMakeFiles/aplicacion4.dir/src/PrimaryGenAction.cc.o"
+  "CMakeFiles/aplicacion4.dir/src/SensitiveDetector.cc.o"
+  "aplicacion4"
+  "aplicacion4.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/aplicacion4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
